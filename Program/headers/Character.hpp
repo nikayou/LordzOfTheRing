612be172f690cee1293d97e4a234868b3e844296
Lasconik <caillouxnicolas@gmail.com>
2013-06-29 20:50:16 +0200

    Started Player.hpp, the class for players
    Signed-off-by: Lasconik <caillouxnicolas@gmail.com>

--- Program/headers/Character.hpp
+++ Program/headers/Character.hpp
@@ -30,28 +30,49 @@ public:
   Character(const unsigned short&, const Stats::stats&, const Stats::stats&, const Stats::stats&, const Stats::stats&, const Stats::stats&);
   ~Character();
   //getters
-  unsigned short getHealth(){ return m_health; };
-  Stats::stats getLResistance(){ return m_LResistance; };
-  Stats::stats getHResistance(){ return m_HResistance; };
-  Stats::stats getStamina(){ return m_stamina; };
-  Stats::stats getLAttack(){ return m_LAttack; };
-  Stats::stats getHAttack(){ return m_HAttack; };
-  Stats::stats getDodging(){ return m_dodging; };
+  unsigned short getHealth() const{ 
+    return m_health; 
+  };
+  Stats::stats getLResistance() const{ 
+    return m_LResistance; 
+  };
+  Stats::stats getHResistance() const{ 
+    return m_HResistance; 
+  };
+  Stats::stats getStamina() const{ 
+    return m_stamina; 
+  };
+  Stats::stats getLAttack() const{
+    return m_LAttack; 
+  };
+  Stats::stats getHAttack() const{ 
+    return m_HAttack; 
+  };
+  Stats::stats getDodging() const{ 
+    return m_dodging; 
+  };
   //setters  
   void setHealth(const unsigned short& value){  
-    m_health = value; };
+    m_health = value; 
+  };
   void setLResistance(const Stats::stats& value){  
-    m_LResistance = value; };
+    m_LResistance = value; 
+  };
   void setHResistance(const Stats::stats& value){  
-    m_HResistance = value; };
+    m_HResistance = value; 
+  };
   void setStamina(const Stats::stats& value){  
-    m_stamina = value; };
+    m_stamina = value; 
+  };
   void setLAttack(const Stats::stats& value){  
-    m_LAttack = value; };
+    m_LAttack = value; 
+  };
   void setHAttack(const Stats::stats& value){  
-    m_HAttack = value; };
+    m_HAttack = value; 
+  };
   void setDodging(const Stats::stats& value){  
-    m_dodging = value; };
+    m_dodging = value; 
+  };
 
 };
 
--- Program/headers/Player.hpp
+++ Program/headers/Player.hpp
@@ -1,5 +1,59 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 
+#include "../headers/Character.hpp"
+
+#include <string>
+
+class Player{
+
+private:
+  std::string m_name; //name of the player, displayed during the match
+  unsigned short m_victories; //number of victories (for global score)
+  unsigned short m_points; //number of points in the current match
+  Character * m_character; //character chosen by the player
+
+public:
+  //constructors
+  Player();
+  ~Player();
+  //getters
+  std::string getName() const{
+    return m_name; 
+  };
+  unsigned short getVictories() const{
+    return m_victories; 
+  };
+  unsigned short getPoints() const {
+    return m_poins; 
+  };
+  Character * getCharacter() const{
+    return m_character; 
+  };
+  //setters
+  void setName(const std::string& value){
+    m_name = value;
+  };
+  void setVictories(const unsigned short& value){
+    m_victories = value;
+  };
+  void setPoints(const unsigned short& value){
+    m_points = value;
+  };
+  void setCharacter(Character * value){
+    m_character = value;
+  };
+  //others
+  bool resetPoints(){
+    m_points = 0;
+  };
+  void addPoints(const unsigned short& value){
+    m_points = ( (m_points+value) <= -1) ? m_points+value : -1;
+  };
+  void addVictory(){
+    m_victories = (m_victories == -1) ? -1 : m_victories+1;
+  }
+};
+
 
 #endif
